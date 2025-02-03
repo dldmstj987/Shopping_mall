7cb79cd7ed58ee86c08ec07ffbbd7f505956e4bb
[Lee EunSeo] <dldmstj987@naver.com>
2025-02-03 18:12:23 +0900

    숙제 끝

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Button, Navbar, Container, Nav} from 'react-bootstrap'
 import data from './data.js';
 
 
+
 function App() {
   let [shoes] = useState(data)
 
@@ -23,28 +24,21 @@ function App() {
     <div className='main-bg'></div>
 
     <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-          <h4>{shoes[0].title}</h4>
-          <p>{shoes[0].price}</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-          <h4>{shoes[1].title}</h4>
-          <p>{shoes[1].price}</p>
-        </div>
-        <div className="col-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-          <h4>{shoes[2].title}</h4>
-          <p>{shoes[2].price}</p>
-        </div>
-      </div>
-
+      <Card shoes={shoes[0]} i={1}/>
+      <Card shoes={shoes[1]} i ={2}/>
+      <Card shoes={shoes[2]} i={3}/>
     </div>
-
-    
   </div>
+  )
+}
+function Card(props) {
+  return(
+    <div className="col-md-4">
+     <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+      <h4>{props.shoes.tilte}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
   )
 }
 
