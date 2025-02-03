e43acaffbbee2691b309d8047f61c484bf44a6a4
[Lee EunSeo] <dldmstj987@naver.com>
2025-02-03 14:40:06 +0900

    네이버바 만듬

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div>
     <Navbar bg="dark" variant="dark">
       <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="#home">space;BAR</Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
