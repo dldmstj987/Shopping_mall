2cada4a390d180c10ae1050752e225e4f5c9d389
[Lee EunSeo] <dldmstj987@naver.com>
2025-02-03 16:29:14 +0900

    import, export 넣음

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import data from './data.js';
 
 
 function App() {
+  let [shoes] = useState(data)
 
   return(
     <div>
@@ -24,18 +26,18 @@ function App() {
       <div className="row">
         <div className="col-md-4">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
+          <h4>{shoes[0].title}</h4>
+          <p>{shoes[0].price}</p>
         </div>
         <div className="col-md-4">
           <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
+          <h4>{shoes[1].title}</h4>
+          <p>{shoes[1].price}</p>
         </div>
         <div className="col-md-4">
           <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
+          <h4>{shoes[2].title}</h4>
+          <p>{shoes[2].price}</p>
         </div>
       </div>
 
---
+++ src/data.js
@@ -0,0 +1,22 @@
+let data = [
+    {
+        id: 0,
+        title: "White and Black",
+        content: "Born in France",
+        price: 120000
+    },
+    {
+        id: 1,
+        title: "Red Knit",
+        content: "Born in Seoul",
+        price: 110000
+    },
+    {
+        id: 2,
+        title: "Grey Yordan",
+        content: "Born in the States",
+        price: 130000
+    }
+]
+
+export default data; // 이렇게 하면 다른 파일에서 import a from './data.js'로 사용 가능
